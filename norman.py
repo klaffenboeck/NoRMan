11eadc8e53f3484635f3074260793b4f617a3f25
Manfred Klaffenboeck <fred_klaf@gmx.net>
2025-02-17 10:14:40 +0100

    rename fun welcome message when launching norman

--- norman.py
+++ norman.py
@@ -1,5 +1,5 @@
 import norman_forms
 
 if __name__ == "__main__":
-    print("Good afternoon everyone! - Ho/How/Hi/Hey Norm!")
+    print("Good afternoon everyone! - Yay/Hi/Hey Norm!")
     norman_forms.main()
